b183114aa8c932c259317744db6480d5f3d541ad
Carlo Segat <segatcarlo95@gmail.com>
2025-10-02 15:27:20 +0200

    feat(common server)
    custom code for server

--- build.sh
+++ build.sh
@@ -4,6 +4,11 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# build server
+(cd "${DIR}"/src/common-server && CGO_ENABLED=0 GOOS=linux go build -v -o "${DIR}"/docker/spire-server-broker.example/common-server)
+(cd "${DIR}"/src/common-server && CGO_ENABLED=0 GOOS=linux go build -v -o "${DIR}"/docker/spire-server-stockmarket.example/common-server)
+
+# build the agents/workloads
 (cd "${DIR}"/src/broker-webapp && CGO_ENABLED=0 GOOS=linux go build -v -o "${DIR}"/docker/broker-webapp/broker-webapp)
 (cd "${DIR}"/src/stock-quotes-service && CGO_ENABLED=0 GOOS=linux go build -v -o "${DIR}"/docker/stock-quotes-service/stock-quotes-service)
 
